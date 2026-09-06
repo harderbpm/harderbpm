9b1789961b52760d3d89b2cf20786be0d525ba05
harderbpm <bpmharder@gmail.com>
2026-09-06 20:03:39 +0200

    Update consent.js

--- consent.js
+++ consent.js
@@ -1,1 +1,40 @@
+const cookieBanner = document.getElementById("cookie-banner");
+const cookieAccept = document.getElementById("cookie-accept");
+const cookieReject = document.getElementById("cookie-reject");
 
+const savedConsent = localStorage.getItem(
+  "harderbpm-cookie-consent"
+);
+
+if (!savedConsent) {
+  cookieBanner.hidden = false;
+}
+
+cookieAccept.addEventListener("click", () => {
+  localStorage.setItem(
+    "harderbpm-cookie-consent",
+    "accepted"
+  );
+
+  gtag("consent", "update", {
+    analytics_storage: "granted"
+  });
+
+  cookieBanner.hidden = true;
+});
+
+cookieReject.addEventListener("click", () => {
+  localStorage.setItem(
+    "harderbpm-cookie-consent",
+    "necessary"
+  );
+
+  gtag("consent", "update", {
+    analytics_storage: "denied",
+    ad_storage: "denied",
+    ad_user_data: "denied",
+    ad_personalization: "denied"
+  });
+
+  cookieBanner.hidden = true;
+});
